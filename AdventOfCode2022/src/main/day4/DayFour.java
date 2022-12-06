d284c4ad3366946fbeba56c367a603dde94f3e5e
Jakob Priesner <jakob.priesner@siemens.com>
2022-12-06 07:54:38 +0100

    feat: solve task 2 of day 4

--- AdventOfCode2022/src/main/day4/DayFour.java
+++ AdventOfCode2022/src/main/day4/DayFour.java
@@ -10,6 +10,7 @@ public class DayFour {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Task 1: " + getCountOfFullyContainedElves());
+        System.out.println("Task 2: " + getCountOfOverlappingPairs());
     }
 
     private static long getCountOfFullyContainedElves() throws IOException {
@@ -18,6 +19,15 @@ public class DayFour {
                 .map(s -> s.split("[-,]"))
                 .map(line -> Arrays.stream(line).mapToInt(Integer::parseInt).toArray())
                 .filter(val -> (val[0] <= val[2] && val[1] >= val[3]) || (val[2] <= val[0] && val[3] >= val[1]))
+                .count();
+    }
+
+    private static long getCountOfOverlappingPairs() throws IOException {
+        return FileHelper.getAllLines(Objects.requireNonNull(DayFour.class.getResource("./input.txt")).getPath())
+                .stream()
+                .map(s -> s.split("[-,]"))
+                .map(line -> Arrays.stream(line).mapToInt(Integer::parseInt).toArray())
+                .filter(val -> !(val[1] < val[2] || val[3] < val[0]))
                 .count();
     }
 
